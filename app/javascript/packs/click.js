8d0f1be8cbf2263d4908e09762fa93436485c276
Sin63 <sincov63@gmail.com>
2020-12-08 23:51:59 +0300

    Add questions_controller

---
+++ app/javascript/packs/click.js
@@ -0,0 +1,16 @@
+// document.addEventListener('turbolinks:load', () => {
+//     const askButton = document.getElementById('ask-button')
+//     const askForm = document.getElementById('ask-form')
+
+//     if (askButton && askForm) {
+//         askButton.addEventListener('click', (event) => {
+//             event.preventDefault()
+//             setTimeout(() => {
+//                 let displayStyle =
+//                     askForm.style.display === 'block' ? 'none' : 'block'
+
+//                 askForm.style.display = displayStyle
+//             }, 300)
+//         })
+//     }
+// })
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
+  root 'users#index'
 
-root 'users#index'
-resources :users, except: [:destroy]
-resources :sessions, only: [:new, :create, :destroy]
-resources :questions
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :questions, except: [:new, :index, :show]
 
-get 'show' => 'users#show'
-
-get 'sign_up' => 'users#new'
-get 'log_out' => 'sessions#destroy'
-get 'log_in' => 'sessions#new'
-  
- 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
