71ac6b63b58fe05a475a08095fa14554b82b8b56
Sin63 <sincov63@gmail.com>
2020-12-09 11:13:00 +0300

    Add changes

--- app/javascript/packs/click.js
+++ app/javascript/packs/click.js
@@ -1,16 +1,16 @@
-// document.addEventListener('turbolinks:load', () => {
-//     const askButton = document.getElementById('ask-button')
-//     const askForm = document.getElementById('ask-form')
+document.addEventListener('turbolinks:load', () => {
+    const askButton = document.getElementById('ask-button')
+    const askForm = document.getElementById('ask-form')
 
-//     if (askButton && askForm) {
-//         askButton.addEventListener('click', (event) => {
-//             event.preventDefault()
-//             setTimeout(() => {
-//                 let displayStyle =
-//                     askForm.style.display === 'block' ? 'none' : 'block'
+    if (askButton && askForm) {
+        askButton.addEventListener('click', (event) => {
+            event.preventDefault()
+            setTimeout(() => {
+                let displayStyle =
+                    askForm.style.display === 'block' ? 'none' : 'block'
 
-//                 askForm.style.display = displayStyle
-//             }, 300)
-//         })
-//     }
-// })
+                askForm.style.display = displayStyle
+            }, 300)
+        })
+    }
+})
